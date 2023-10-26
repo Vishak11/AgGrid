974a647770f60c5554926a2c754ecff8d2e06916
Vishak11 <vishakvn007@gmail.com>
2023-10-26 11:08:06 +0530

    v3(search)

--- src/Aggrid.jsx
+++ src/Aggrid.jsx
@@ -29,7 +29,8 @@ const Aggrid = () => {
         fetch('https://jsonplaceholder.typicode.com/comments')
             .then(resp => resp.json())
             .then(resp => {
-                params.api.applyTransaction({ add: resp });
+                 params.api.applyTransaction({ add: resp });
+                 setColumnData(params.api)
                 // setColumnData(params.columnApi);
             });
     };
@@ -44,10 +45,15 @@ const Aggrid = () => {
         setHideC(!hideC);
         
     };
+    const searchData=(e)=>{
+        columnData.setQuickFilter(e.target.value)
+        console.log("c",columnData)
+    }
 
     return (
         <div className="ag-theme-alpine" style={{ height: '100vh', width: '100vw' }}>
             <button onClick={showData}>ShowData</button>
+            <input onChange={searchData} placeholder="Search"></input>
             <div style={{ height: '100%', width: '100%' }}>
                 <AgGridReact
                     columnDefs={columnDefs}
